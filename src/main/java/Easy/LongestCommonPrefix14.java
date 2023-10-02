78d5de86272ffb61b53d7f160e5d76b1f580303d
Hlib Astashev <hlibastashev@gmail.com>
2023-10-02 05:19:40 +0200

    new tasks added

--- src/main/java/Easy/LongestCommonPrefix14.java
+++ src/main/java/Easy/LongestCommonPrefix14.java
@@ -5,24 +5,18 @@ public class LongestCommonPrefix14 {
         if (strs == null || strs.length == 0) {
             return "";
         }
-
         String commonPrefix = strs[0];
-
         for (int i = 1; i < strs.length; i++) {
             String currentWord = strs[i];
             int j = 0;
-
             while (j < commonPrefix.length() && j < currentWord.length() && commonPrefix.charAt(j) == currentWord.charAt(j)) {
                 j++;
             }
-
             commonPrefix = commonPrefix.substring(0, j);
-
             if (commonPrefix.isEmpty()) {
                 return "";
             }
         }
-
         return commonPrefix;
     }
 }
